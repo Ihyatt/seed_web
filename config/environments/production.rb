b4071f312c360eb9b9f025ed30da762abf7a96f2
Ketan Anjaria <fire@kidbombay.com>
2016-08-12 14:17:31 -0700

    added devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,6 +52,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "seed_web_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'seed-web.herokuapp.com', port: 3000 }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
