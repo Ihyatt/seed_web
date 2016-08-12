0e223294dfdd108a2c560d9c2193b31a5b21fba2
Ketan Anjaria <fire@kidbombay.com>
2016-08-12 14:26:50 -0700

    added rolify

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'haml-rails', '~> 0.9'
 gem 'haml'
 gem 'devise'
 gem 'bootstrap', '~> 4.0.0.alpha3.1'
+gem 'rolify'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
+  # Extensions
+  rolify
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  after_initialize :setup, :if => :new_record?
+
+  def setup
+    set_default_role
+  end
+
+  def set_default_role
+    add_role :normal
+  end
 end
