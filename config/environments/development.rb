9c58958a498d5015b0ae0ae24843b007506bb6c4
Ketan Anjaria <fire@kidbombay.com>
2016-08-13 13:18:05 -0700

    added dot env gem to load vars from .env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
+require 'dotenv'
 Rails.application.configure do
+  Dotenv.load
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
