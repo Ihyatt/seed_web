a26ca9fe131ac8da8863d3ce146a478c20d5dee5
Ketan Anjaria <fire@kidbombay.com>
2016-08-13 13:33:08 -0700

    add sidekiq web ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,23 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
-  namespace :api, :defaults => {:format => :json} do
-    namespace :v1 do
-      resources :users
-    end
+  authenticate :user do
+    mount Sidekiq::Web => '/sidekiq'
   end
-  
+
+  resources :conversations
   devise_for :users,
     path:        '',
     path_names:  {:sign_in => 'login', :sign_out => 'logout', :edit => 'settings'},
     controllers: {registrations: 'registrations'}
 
   get 'about' => 'pages#about', as: :about
+
+  namespace :api, :defaults => {:format => :json} do
+    namespace :v1 do
+      resources :users
+    end
+  end
+
   root to: 'pages#index'
 end
