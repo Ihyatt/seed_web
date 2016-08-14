353be19590313defb83ca4d71b136363dcecd67c
Ketan Anjaria <fire@kidbombay.com>
2016-08-14 14:43:54 -0700

    added support for 404 in api

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,3 +1,11 @@
 class API::V1::APIController < ActionController::API
-  
+  rescue_from ActiveRecord::RecordNotFound, :with => :catch_404
+
+  def catch_404(exception)
+    render_error(exception.message, 404)
+  end  
+
+  def render_error(message, status)
+    render json: { success: false, status: status, errors: [ message: message ]}
+  end
 end
