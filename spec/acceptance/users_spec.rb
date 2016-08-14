351f1f06b5f14b3eef2e55798aa95537b287e37b
Ketan Anjaria <fire@kidbombay.com>
2016-08-14 13:37:36 -0700

    added api get users spec

--- spec/acceptance/users_spec.rb
+++ spec/acceptance/users_spec.rb
@@ -22,4 +22,23 @@ resource "Users" do
       #json.should == ""
     end
   end
+
+  get "/api/v1/users/:id" do
+    parameter :id, "User ID", required: true
+
+    example "Get A User" do
+      do_request(id: user.id)
+
+      expect(status).to eq(200)
+      
+      json = JSON.parse(response_body)
+      #json.should == ""
+
+      user_json = json
+      expect(user_json["id"]).to eq(user.id)
+      expect(user_json["uid"]).to eq(user.uid)
+      expect(user_json["created_at"]).not_to be_nil
+      expect(user_json["updated_at"]).not_to be_nil
+    end
+  end
 end
