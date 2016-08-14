cb13573b3cca7598d501ea7baf05a623b7b92f7a
Ketan Anjaria <fire@kidbombay.com>
2016-08-14 13:35:44 -0700

    added basic users_spec

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'sidekiq'
 gem 'rack-protection', github: 'sinatra/rack-protection', require: false
 gem 'sinatra', github: 'sinatra', require: false
 gem 'active_model_serializers', '~> 0.10.0'
+gem 'rspec_api_documentation'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ spec/acceptance/users_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+require 'rspec_api_documentation/dsl'
+
+resource "Users" do
+  let(:user) { FactoryGirl.create(:user)}
+  get "/api/v1/users" do
+    example "Get All Users" do
+      user
+      do_request
+
+      expect(status).to eq(200)
+      
+      json = JSON.parse(response_body)
+      expect(json.count).to eq(1)
+
+      user_json = json[0]
+      expect(user_json["id"]).to eq(user.id)
+      expect(user_json["uid"]).to eq(user.uid)
+      expect(user_json["created_at"]).not_to be_nil
+      expect(user_json["updated_at"]).not_to be_nil
+
+      #json.should == ""
+    end
+  end
+end
