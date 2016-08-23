ac83859dc89772ef3b3ac29e653ed485a908628e
Baraka Kombe-Jarvis <theforestgreenbart@gmail.com>
2016-08-23 13:43:29 -0400

    api key changes

---
+++ db/migrate/20160814192911_api_key.rb
@@ -0,0 +1,4 @@
+class APIKey < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- spec/models/api_key_spec.rb
+++ spec/models/api_key_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe APIKey, type: :model do
+<<<<<<< Updated upstream
   let(:api_key) { FactoryGirl.build(:api_key) }
 
   describe "associations" do
@@ -23,4 +24,7 @@ RSpec.describe APIKey, type: :model do
       puts api_key.inspect
     end
   end
+=======
+  pending "add some examples to (or delete) #{__FILE__}"
+>>>>>>> Stashed changes
 end
