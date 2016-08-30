a651a4d6d6b1ac8bdcdec00c3af5c5316e5b33fc
Baraka Kombe-Jarvis <theforestgreenbart@gmail.com>
2016-08-30 13:02:14 -0400

    api key changes

--- spec/factories/api_keys.rb
+++ spec/factories/api_keys.rb
@@ -1,12 +1,11 @@
 FactoryGirl.define do
   factory :api_key do
-<<<<<<< Updated upstream
     read_key SecureRandom.hex(16)
     write_key SecureRandom.hex(16)
     user
-=======
-    read_key "MyString"
-    write_key "MyString"
->>>>>>> Stashed changes
+# =======
+#     read_key "MyString"
+#     write_key "MyString"
+# >>>>>>> Stashed changes
   end
 end
