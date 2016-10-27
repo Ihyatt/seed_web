8943631e66d979796612e2d11ac01eca319375c7
Khadijah Coakley <khadijah@wildfyregroup.com>
2016-10-26 19:49:14 -0500

    mobile bugs fixed. unused favicons removed

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,31 @@
 (function($) {
 var App = { init: function() { App.HomepageHeight();           // helper script to set homepage to full height
                                App.HomepageOpacity();          // changes homepage opacity on scroll
-							   App.MaxImage_Video();           // homepage background - video
-							   App.MaxImage_Slider();          // homepage background - image slider
-							   App.MaxImage_Single();          // homepage background - vingle image
-							   App.ScrollToSomeplace();        // script resposible for smooth scrolling after clicking on menu item
-							   App.FormValidation();           // newsletter form validation
-							   App.ContactForm();              // contact form
-							   App.Fit_Vids();                 // responsive videos
-							   App.Stellar();                  // parallax effect
-							   App.Reviews();                  // reviews carousel
-							   App.Screenshots_Carousel();     // screenshots carousel 
-							   App.Nivo_Lightbox();            // lightbox
-							   App.Elements_animation();       // animations
-							   },
+                               App.MaxImage_Video();           // homepage background - video
+                               App.MaxImage_Slider();          // homepage background - image slider
+                               App.MaxImage_Single();          // homepage background - vingle image
+                               App.ScrollToSomeplace();        // script resposible for smooth scrolling after clicking on menu item
+                               App.FormValidation();           // newsletter form validation
+                               App.ContactForm();              // contact form
+                               App.Fit_Vids();                 // responsive videos
+                               App.Stellar();                  // parallax effect
+                               App.Reviews();                  // reviews carousel
+                               App.Screenshots_Carousel();     // screenshots carousel 
+                               App.Nivo_Lightbox();            // lightbox
+                               App.Elements_animation();       // animations
+                               },
 
-	// Homepage Height
-	HomepageHeight: function() {
-	"use strict"; 
-	    var h = window.innerHeight;
-	    $('.hero_fullscreen').css('height', h );
-	    $('.mockup').css('height', h );
-	},
-	
-	
-	// Homepage Opacity - for single background image version
-	HomepageOpacity: function() {
+    // Homepage Height
+    HomepageHeight: function() {
+    "use strict"; 
+        var h = window.innerHeight;
+        $('.hero_fullscreen').css('height', h );
+        $('.mockup').css('height', h );
+    },
+    
+    
+    // Homepage Opacity - for single background image version
+    HomepageOpacity: function() {
     "use strict"; 
         var h = window.innerHeight;
         $(window).on('scroll', function() {
@@ -33,43 +33,43 @@ var App = { init: function() { App.HomepageHeight();           // helper script 
             $('#maximage_single').css('opacity', (1-st/h) );
         });
     },
-	
-	
-	// MaxImage Fullscreen Background Slider
-	MaxImage_Video: function() {
-	"use strict";
-	    jQuery('video, object').maximage('maxcover');
-	    
-	    // detecting if browser is mobile and disabling the video background, leaving only poster as bg.
-		if( navigator.userAgent.match(/Android/i)
-		|| navigator.userAgent.match(/webOS/i)
-		|| navigator.userAgent.match(/iPhone/i)
-		|| navigator.userAgent.match(/iPad/i)
-		|| navigator.userAgent.match(/iPod/i)
-		|| navigator.userAgent.match(/BlackBerry/i)
-		|| navigator.userAgent.match(/Windows Phone/i)
-		 ){
-		    $('#maximage_video video').css('display', 'none' );
-		    classie.add( document.getElementById( 'maximage_video' ), 'mobile_novideo' );
-		  };
-	},
-	
-	
-	// MaxImage Fullscreen Background Slider
-	MaxImage_Slider: function() {
-	"use strict"; 
-	    $('#maximage_slider').maximage();
-	},
-	
-	
-	// MaxImage Fullscreen Background Slider
-	MaxImage_Single: function() {
-	"use strict";
-	    $('#maximage_single').maximage();
-	},
-	
-	
-	// Scroll To ...
+    
+    
+    // MaxImage Fullscreen Background Slider
+    MaxImage_Video: function() {
+    "use strict";
+        jQuery('video, object').maximage('maxcover');
+        
+        // detecting if browser is mobile and disabling the video background, leaving only poster as bg.
+        if( navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)
+         ){
+            $('#maximage_video video').css('display', 'none' );
+            classie.add( document.getElementById( 'maximage_video' ), 'mobile_novideo' );
+          };
+    },
+    
+    
+    // MaxImage Fullscreen Background Slider
+    MaxImage_Slider: function() {
+    "use strict"; 
+        $('#maximage_slider').maximage();
+    },
+    
+    
+    // MaxImage Fullscreen Background Slider
+    MaxImage_Single: function() {
+    "use strict";
+        $('#maximage_single').maximage();
+    },
+    
+    
+    // Scroll To ...
     ScrollToSomeplace: function() {
     $('.go_to_home').click(function () {$.scrollTo('.hero_fullscreen',1000,{easing:'easeInOutExpo','axis':'y'});return false}); 
     
@@ -114,37 +114,37 @@ var App = { init: function() { App.HomepageHeight();           // helper script 
     },
     
     
-	// Newsletter Form Validation
+    // Newsletter Form Validation
     FormValidation: function() {
-	    function isValidEmailAddress(emailAddress) {
-	    var pattern = new RegExp(/^(("[\w-+\s]+")|([\w-+]+(?:\.[\w-+]+)*)|("[\w-+\s]+")([\w-+]+(?:\.[\w-+]+)*))(@((?:[\w-+]+\.)*\w[\w-+]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][\d]\.|1[\d]{2}\.|[\d]{1,2}\.))((25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\.){2}(25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\]?$)/i);
-	    return pattern.test(emailAddress);
-		};
+        function isValidEmailAddress(emailAddress) {
+        var pattern = new RegExp(/^(("[\w-+\s]+")|([\w-+]+(?:\.[\w-+]+)*)|("[\w-+\s]+")([\w-+]+(?:\.[\w-+]+)*))(@((?:[\w-+]+\.)*\w[\w-+]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][\d]\.|1[\d]{2}\.|[\d]{1,2}\.))((25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\.){2}(25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\]?$)/i);
+        return pattern.test(emailAddress);
+        };
     
-	    $('#subscribe-form').isHappy({
-	    submitButton: '#subscribe-form-submit',
-	    fields: {
-	      '#email': {
-	        required: true,
-	        message: 'Please enter a valid e-mail address!',
-	        test: isValidEmailAddress
-	      }
-	    }
-		});
+        $('#subscribe-form').isHappy({
+        submitButton: '#subscribe-form-submit',
+        fields: {
+          '#email': {
+            required: true,
+            message: 'Please enter a valid e-mail address!',
+            test: isValidEmailAddress
+          }
+        }
+        });
     },
     
     
     // Contact Form
     ContactForm: function() {
-	     "use strict";
-	    var options = {target: "#alert"}
-	    $("#contact-form").ajaxForm(options);
+         "use strict";
+        var options = {target: "#alert"}
+        $("#contact-form").ajaxForm(options);
     },
     
     
     // Elements Animation
     Elements_animation: function() {
-		$('#meet-raheem').waypoint(function() {
+        $('#meet-raheem').waypoint(function() {
             setTimeout(function(){$('.content_anim1').addClass('animated fadeInUp')},300);
             setTimeout(function(){$('.content_anim2').addClass('animated fadeInUp')},600);
             setTimeout(function(){$('.content_anim3').addClass('animated fadeInUp')},900);
@@ -161,7 +161,7 @@ var App = { init: function() { App.HomepageHeight();           // helper script 
             setTimeout(function(){$('.content_anim7').addClass('animated fadeInUp')},1000);
         }, { offset: '50%' });    
     
-    	
+        
         $('#community-impact').waypoint(function() {
             setTimeout(function(){$('.content_anim8').addClass('animated fadeInUp')},0);
             setTimeout(function(){$('.content_anim9').addClass('animated fadeInUp')},200);
@@ -182,52 +182,52 @@ var App = { init: function() { App.HomepageHeight();           // helper script 
     
     // Nivo Lightbox
     Nivo_Lightbox: function() {
-	    $('.play a, .owl-carousel-screenshots a').nivoLightbox({
-		    effect: 'slideDown'    
-	    });	
+        $('.play a, .owl-carousel-screenshots a').nivoLightbox({
+            effect: 'slideDown'    
+        }); 
     },
     
     
     // Fit Vids
     Fit_Vids: function() {
-	    $("#video_modal_box").fitVids();	
+        $("#video_modal_box").fitVids();    
     },
     
     
     // Parallaxed Backgrounds
     Stellar: function() {
     
-    	// detecting if browser is mobile and disabling the video background, leaving only poster as bg.
-		if( navigator.userAgent.match(/Android/i)
-		|| navigator.userAgent.match(/webOS/i)
-		|| navigator.userAgent.match(/iPhone/i)
-		|| navigator.userAgent.match(/iPad/i)
-		|| navigator.userAgent.match(/iPod/i)
-		|| navigator.userAgent.match(/BlackBerry/i)
-		|| navigator.userAgent.match(/Windows Phone/i)
-		 ){} else {
-			 $(window).stellar({responsive: true});	 
-		 };    	
+        // detecting if browser is mobile and disabling the video background, leaving only poster as bg.
+        if( navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)
+         ){} else {
+             $(window).stellar({responsive: true});  
+         };     
     },
     
     // Reviews Carousel
     Reviews: function() {
-	    $(".owl-carousel").owlCarousel({
-		    loop:true,
-		    singleItem : true,
-	    });	
+        $(".owl-carousel").owlCarousel({
+            loop:true,
+            singleItem : true,
+        }); 
     },
     
     // Screenshots Carousel
     Screenshots_Carousel: function() {
-	    $(".owl-carousel-screenshots").owlCarousel({
-			loop:true,
-		    items : 7,
-			itemsDesktop : [1200,7], 
-			itemsDesktopSmall : [900,6], 
-			itemsTablet: [600,4], 
-			itemsMobile : [400,2]     
-	    });	
+        $(".owl-carousel-screenshots").owlCarousel({
+            loop:true,
+            items : 7,
+            itemsDesktop : [1200,7], 
+            itemsDesktopSmall : [900,6], 
+            itemsTablet: [600,4], 
+            itemsMobile : [400,2]     
+        }); 
     },    
 
 }
