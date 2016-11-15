240405b57dd5268296e6d0cb0c397efd12b1ec65
Ketan Anjaria <fire@kidbombay.com>
2016-11-15 03:59:27 -0800

    use cloudinary for image upload

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ gem 'fast_seeder', :github => 'kidbombay/fast_seeder'
 gem 'rails_admin', '~> 1.0'
 gem 'rollbar'
 gem 'paperclip', '~> 5.0.0'
+gem 'paperclip-cloudinary'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,8 @@
 class Attachment < ApplicationRecord
   # Extensions
-  has_attached_file :asset, styles: { thumb: "100x100>" }
+  has_attached_file :asset, styles: { thumb: "100x100>" },
+    :storage => :cloudinary,
+    :path => ':rails_env_:class_:attachment_:id.:extension'
 
   # Associations
   belongs_to :incident
