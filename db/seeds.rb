fdd5d196652f441bf782ff439d2a87a1527f0a0c
Ketan Anjaria <fire@kidbombay.com>
2016-11-14 17:24:58 -0800

    seed Gender

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,6 @@ user = CreateUserService.create_admin_user('fire@kidbombay.com', 'testtest')
 user = CreateUserService.create_admin_user('brandon@theswatapp.co', 'testtest')
 
 Race.seed
+Gender.seed
 
 Place.import_cities
