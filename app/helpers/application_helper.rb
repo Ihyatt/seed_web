48a499e73e1afce56ff3d591427dd075e83b1cae
Ketan Anjaria <fire@kidbombay.com>
2016-11-15 05:40:25 -0800

    add model_image helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def model_image(image, width, height, use_https = false)
+    if image
+      image.remote_url(width: width, height: height, crop: :fill, secure: use_https, gravity: :north, format: "jpg", quality: 80)
+    else
+      image_placeholder_url(width,height)
+    end
+  end
+
+  def image_placeholder_url(width, height)
+    return "//placehold.it/#{width}x#{height}"
+  end
 end
