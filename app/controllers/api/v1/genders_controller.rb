d8387994b5ba137f9831c4e5cab5263f445910c1
Ketan Anjaria <fire@kidbombay.com>
2016-11-15 06:39:05 -0800

    return gender and race by position in api

--- app/controllers/api/v1/genders_controller.rb
+++ app/controllers/api/v1/genders_controller.rb
@@ -1,7 +1,7 @@
 class API::V1::GendersController < API::V1::APIController
 
   def index
-    scope = Gender.all
+    scope = Gender.by_position
     @genders = scope.page(params[:page])
     render_collection(@genders, scope)
   end
--- app/controllers/api/v1/races_controller.rb
+++ app/controllers/api/v1/races_controller.rb
@@ -1,7 +1,7 @@
 class API::V1::RacesController < API::V1::APIController
 
   def index
-    scope = Race.all
+    scope = Race.by_position
     @races = scope.page(params[:page])
     render_collection(@races, scope)
   end
